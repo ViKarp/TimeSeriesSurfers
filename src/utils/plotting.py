0676ecd2af35b3f60435bed25e123be573bc35e8
ViKarp <vikarp21@gmail.com>
2024-08-02 00:26:30 +0300

    add file plotting.py

---
+++ src/utils/plotting.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+def plot_results(real_data, predicted_data):
+    pass
+    # TODO: plot predict and real time series (maybe such as sktime)
+
+def plot_trigger_list(trigger_list):
+    pass
+    # TODO: plot trigger values.
